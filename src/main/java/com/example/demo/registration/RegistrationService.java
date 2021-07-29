727d31460aa975a9b412e3c223ece95c321eb182
Cosinus <cheikh.s@makeba.money>
2021-07-29 15:36:08 +0000

    create confirmation token

--- src/main/java/com/example/demo/registration/RegistrationService.java
+++ src/main/java/com/example/demo/registration/RegistrationService.java
@@ -1,5 +1,8 @@
 package com.example.demo.registration;
 
+import com.example.demo.appuser.AppUser;
+import com.example.demo.appuser.AppUserRole;
+import com.example.demo.appuser.AppUserService;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -10,7 +13,22 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class RegistrationService {
 
+    private final AppUserService appUserService;
+    private final EmailValidator emailValidator;
+
     public String register(RegistrationRequest request) {
-        return "works!";
+        boolean isValidEmail = emailValidator.test(request.getEmail());
+        if(!isValidEmail) {
+            throw new IllegalStateException("email not valid");
+        }
+        return appUserService.signUpUser(
+                new AppUser(
+                        request.getFirstName(),
+                        request.getLastName(),
+                        request.getEmail(),
+                        request.getPassword(),
+                        AppUserRole.USER
+                )
+        );
     }
 }
